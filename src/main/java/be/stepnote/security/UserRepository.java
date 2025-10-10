56fdfc603e9da1150ffdc6f4545c3b30cd349398
cloud <goorm94@naver.com>
2025-10-11 08:10:43 +0900

    chore: 초기 설정 세팅4

---
+++ src/main/java/be/stepnote/security/UserRepository.java
@@ -0,0 +1,11 @@
+package be.stepnote.security;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<UserEntity, Integer> {
+
+
+    Boolean existsByUsername(String username);
+
+    UserEntity findByUsername(String username);
+}
