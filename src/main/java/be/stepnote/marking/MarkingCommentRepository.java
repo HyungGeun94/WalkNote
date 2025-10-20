c81bc72d1d41e48bb126df67b4b2b6fa624ce87e
cloud <goorm94@naver.com>
2025-10-20 15:45:38 +0900

    remove: 위,경도 수집 문제로 인한 기능 삭제

--- src/main/java/be/stepnote/marking/MarkingCommentRepository.java
+++
@@ -1,7 +0,0 @@
-package be.stepnote.marking;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface MarkingCommentRepository extends JpaRepository<MarkingComment, Long>,MarkingCommentCustomRepository{
-
-}
